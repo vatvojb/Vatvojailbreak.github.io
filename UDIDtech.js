50cffd35237e8cce6870481d7848b8258d607762
Vật Vờ Jailbreak <143106241+vatvojb@users.noreply.github.com>
2023-09-06 06:32:46 +0700

    Update UDIDtech.js

--- UDIDtech.js
+++ UDIDtech.js
@@ -56,3 +56,59 @@ export class UDIDTech {
       return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
   }
 }
+
+require 'faraday'
+
+module UDIDTech
+  module Client
+
+    def self.connection
+      @connection ||= Faraday.new(
+        url: Rails.configuration.api_udid_tech[:url],
+        headers: { 'Authorization' => "Token #{Rails.configuration.api_udid_tech[:token]}" },
+        ssl: { verify: false }
+      )
+    end
+  end
+end
+
+require 'faraday'
+require 'json'
+require 'udid_tech/client'
+
+class ConnectionsController < ApplicationController
+  def get_profile
+    response = UDIDTech::Client.connection.get("/recognition/profile/?redirect_url=#{request.protocol}#{[80, 443].include?(request.port) ? request.host : request.host_with_port}")
+    send_data response.body, type: 'application/x-apple-aspen-config', disposition: 'attachment; filename="udid.mobileconfig"'
+  end
+
+  def get_profile_from_api
+    response = UDIDTech::Client.connection.get("/result/#{User.first.id}/?redirect_url=#{request.protocol}#{[80, 443].include?(request.port) ? request.host : request.host_with_port}/connections/parse_profile_body/")
+    send_data response.body, type: 'application/x-apple-aspen-config', disposition: 'attachment; filename="udid.mobileconfig"'
+  end
+
+  def parse_profile_body
+    conn = Faraday.new(
+      url: Rails.configuration.api_udid_tech[:url],
+      headers: { 'Authorization' => "Api-Key #{Rails.configuration.api_udid_tech[:key]}" },
+      ssl: { verify: false }
+    ) do |faraday|
+      faraday.request :multipart
+      faraday.response :logger
+    end
+
+    body = request.body.string
+    conn.post("/recognize/#{User.first.id}/") do |req|
+      req.headers['Content-Type'] = 'application/x-apple-aspen-config'
+      req.headers['Content-Length'] = body.size.to_s
+      req.body = body
+    end
+
+    redirect_to :root, :status => :moved_permanently
+  end
+
+  def get_result_from_udid_api
+    response = UDIDTech::Client.connection.get("/udid/#{User.first.id}/")
+    render json: JSON.parse(response.body)['result']
+  end
+end
